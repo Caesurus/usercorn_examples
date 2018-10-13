ef9a3950fd5147496738541082d785e56d070e8f
Caesurus <caesurus@gmail.com>
2018-10-13 15:21:39 -0400

    update to not have to explicitly use type

--- 2018_flareon_six_magic/010-dump_functions.lua
+++ 2018_flareon_six_magic/010-dump_functions.lua
@@ -18,7 +18,7 @@ on code 0x402f06 do
   print "Function Length: %#x" %{addr_end-addr_start}
   -- print the function
   dis addr_start addr_end-addr_start
-  print string.rep("-", 70)
+  print ('-'):rep(70)
 end
 
 -- This is the instruction after the decoded function returns.
@@ -34,7 +34,7 @@ on code 0x403b23 do
   --   rax is the retun from fgets() which should be the ptr to the buffer
 
   -- Fill in the buffer with '-'*0x80
-  ptr_buffer = string.rep("-", 70)
+  ptr_buffer = ('-'):rep(70)
   -- Write this string to the buffer, this will update the memory
   write rdi ptr_buffer
 
@@ -45,3 +45,4 @@ on code 0x403b23 do
 
   -- Proceed without being bothered to enter password
 end
+
